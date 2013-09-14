3058ea6171f26e3226fbb8de3db411081e6e4c28
Mathieu Jobin <mjobin@mdsol.com>
2013-09-14 11:11:04 -0400

    reenable and add comments

--- lib/kender/commands/reek.rb
+++ lib/kender/commands/reek.rb
@@ -2,10 +2,12 @@ module Kender
   class Reek < Command
 
     def available?
-      false # true # I'll try running it and not-fail if its not there
+      #TODO: find a better to know if the executable is available.
+      true # I'll try running it and not-fail if its not there
     end
 
     def command
+      # doing simple echo to ignore error status.
       'reek app lib; echo ok'
     end
 
