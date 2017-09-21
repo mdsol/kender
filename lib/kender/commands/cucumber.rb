9e4023a828977f38e79ed275e567174e2b7c118a
Robert Bloor <rbloor@mdsol.com>
2017-09-21 14:18:24 -0400

    debug statements

--- lib/kender/commands/cucumber.rb
+++ lib/kender/commands/cucumber.rb
@@ -8,6 +8,9 @@ module Kender
     def command
       extra_env = ENV['HEADED_BROWSER'] ? "HEADED_BROWSER=#{ENV['HEADED_BROWSER']}" : ''
       if defined?(Knapsack)
+        puts "starting knapsack!"
+        puts "ENV['CI_NODE_TOTAL']: #{ENV['CI_NODE_TOTAL']}"
+        puts "ENV['CI_NODE_INDEX']: #{ENV['CI_NODE_INDEX']}"
         "CI_NODE_TOTAL=#{ENV['CI_NODE_TOTAL']} CI_NODE_INDEX=#{ENV['CI_NODE_INDEX']} bundle exec rake knapsack:cucumber"
       elsif defined?(ParallelTests)
         "#{extra_env} bundle exec rake parallel:features"
