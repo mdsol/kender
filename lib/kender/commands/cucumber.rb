f37bb8a5d40b4c4d09fa254a1b7b218d33e8ac67
Robert Bloor <rbloor@mdsol.com>
2017-09-22 10:41:10 -0400

    remove rails.logger

--- lib/kender/commands/cucumber.rb
+++ lib/kender/commands/cucumber.rb
@@ -8,18 +8,12 @@ module Kender
     def command
       extra_env = ENV['HEADED_BROWSER'] ? "HEADED_BROWSER=#{ENV['HEADED_BROWSER']}" : ''
       if defined?(Knapsack)
-        Rails.logger.error "starting Knapsack!"
-        Rails.logger.info "starting Knapsack!"
         puts "starting Knapsack!"
         "CI_NODE_TOTAL=#{ENV['CI_NODE_TOTAL']} CI_NODE_INDEX=#{ENV['CI_NODE_INDEX']} bundle exec rake knapsack:cucumber"
       elsif defined?(ParallelTests)
-        Rails.logger.error "starting ParallelTests!"
-        Rails.logger.info "starting ParallelTests!"
         puts "starting ParallelTests!"
         "#{extra_env} bundle exec rake parallel:features"
       else
-        Rails.logger.error "starting cucumber!"
-        Rails.logger.info "starting cucumber!"
         puts "starting cucumber!"
         "#{extra_env} bundle exec cucumber"
       end
