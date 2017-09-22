72d11162231f5c5cbd30f823f58abb3ebbc2a860
Robert Bloor <rbloor@mdsol.com>
2017-09-22 12:02:58 -0400

    add headed browser env to knapsack

--- lib/kender/commands/cucumber.rb
+++ lib/kender/commands/cucumber.rb
@@ -8,7 +8,8 @@ module Kender
     def command
       extra_env = ENV['HEADED_BROWSER'] ? "HEADED_BROWSER=#{ENV['HEADED_BROWSER']}" : ''
       if defined?(Knapsack)
-        "CI_NODE_TOTAL=#{ENV['CI_NODE_TOTAL']} CI_NODE_INDEX=#{ENV['CI_NODE_INDEX']} bundle exec rake knapsack:cucumber"
+        knapsack_env = "CI_NODE_TOTAL=#{ENV['CI_NODE_TOTAL']} CI_NODE_INDEX=#{ENV['CI_NODE_INDEX']}"
+        "#{extra_env} #{knapsack_env} bundle exec rake knapsack:cucumber"
       elsif defined?(ParallelTests)
         "#{extra_env} bundle exec rake parallel:features"
       else
