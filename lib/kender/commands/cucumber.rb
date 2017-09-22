4e363d1f67785f4a0b9ae5409128d52b3f77999a
Robert Bloor <rbloor@mdsol.com>
2017-09-22 10:11:01 -0400

    uncomment

--- lib/kender/commands/cucumber.rb
+++ lib/kender/commands/cucumber.rb
@@ -8,10 +8,10 @@ module Kender
     def command
       extra_env = ENV['HEADED_BROWSER'] ? "HEADED_BROWSER=#{ENV['HEADED_BROWSER']}" : ''
       if defined?(Knapsack)
-        # Rails.logger.error "starting knapsack!"
-        # Rails.logger.error "ENV['CI_NODE_TOTAL']: #{ENV['CI_NODE_TOTAL']}"
-        # Rails.logger.error "ENV['CI_NODE_INDEX']: #{ENV['CI_NODE_INDEX']}"
-        # "CI_NODE_TOTAL=#{ENV['CI_NODE_TOTAL']} CI_NODE_INDEX=#{ENV['CI_NODE_INDEX']} bundle exec rake knapsack:cucumber"
+        Rails.logger.error "starting knapsack!"
+        Rails.logger.error "ENV['CI_NODE_TOTAL']: #{ENV['CI_NODE_TOTAL']}"
+        Rails.logger.error "ENV['CI_NODE_INDEX']: #{ENV['CI_NODE_INDEX']}"
+        "CI_NODE_TOTAL=#{ENV['CI_NODE_TOTAL']} CI_NODE_INDEX=#{ENV['CI_NODE_INDEX']} bundle exec rake knapsack:cucumber"
       elsif defined?(ParallelTests)
         "#{extra_env} bundle exec rake parallel:features"
       else
