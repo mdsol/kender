5f1a64cd3e769966910e8b7f558a61f7277114a6
Sean Kwon <skwon@mdsol.com>
2019-01-30 13:33:48 -0500

    with retry command

--- lib/kender/commands/cucumber.rb
+++ lib/kender/commands/cucumber.rb
@@ -7,15 +7,15 @@ module Kender
 
     def command
       extra_env = ENV['HEADED_BROWSER'] ? "HEADED_BROWSER=#{ENV['HEADED_BROWSER']}" : ''
+      retry_command = ENV['CUCUMBER_RETRY'] ? ' --profile rerun' : ''
       if defined?(Knapsack)
         knapsack_env = "CI_NODE_TOTAL=#{ENV['CI_NODE_TOTAL']} CI_NODE_INDEX=#{ENV['CI_NODE_INDEX']}"
-        "#{extra_env} #{knapsack_env} bundle exec rake knapsack:cucumber"
+        "#{extra_env} #{knapsack_env} bundle exec rake knapsack:cucumber#{retry_command}"
       elsif defined?(ParallelTests)
         "#{extra_env} bundle exec rake parallel:features"
       else
-        "#{extra_env} bundle exec cucumber"
+        "#{extra_env} bundle exec cucumber#{retry_command}"
       end
     end
-
   end
 end
